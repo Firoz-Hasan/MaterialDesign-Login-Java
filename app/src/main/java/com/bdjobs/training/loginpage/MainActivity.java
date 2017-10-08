1c67681159892f9e6f3e287b55621c4d2a5fc686
Firoz Hasan <diganto007@users.noreply.github.com>
2017-10-09 02:00:50 +0600

    at home

--- app/src/main/java/com/bdjobs/training/loginpage/MainActivity.java
+++ app/src/main/java/com/bdjobs/training/loginpage/MainActivity.java
@@ -3,11 +3,14 @@ package com.bdjobs.training.loginpage;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText userName, password;
     TextInputLayout userIL, passIL;
+    TextView t1, t2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         passIL = (TextInputLayout) findViewById(R.id.passwordWrapper);
         userIL.setHint("Sign in with email");
         passIL.setHint("Password");
+        t1 = (TextView) findViewById(R.id.textView);
+        t1.setText(Html.fromHtml("Don't have an account? <B>Sign up!<B/>"));
+
 
     }
 }
